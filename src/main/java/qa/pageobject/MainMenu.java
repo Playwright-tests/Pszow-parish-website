bf4df97902ebc6c1e7a46f7bf01b8ce1b00a3acc
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-03-01 19:20:17 +0100

    Added the Allure annotations

--- src/main/java/qa/pageobject/MainMenu.java
+++ src/main/java/qa/pageobject/MainMenu.java
@@ -17,36 +17,43 @@ public class MainMenu extends Menu {
         super(page);
     }
 
+    @io.qameta.allure.Step("Click the \"Ogłoszenia duszpasterskie\" link")
     public void clickAnnouncementsLink() {
 
         clickLink(ANNOUNCEMENTS_SELECTOR);
     }
 
+    @io.qameta.allure.Step("Click the \"Intencje mszalne\" link")
     public void clickIntentionsLink() {
 
         clickLink(INTENTIONS_SELECTOR);
     }
 
+    @io.qameta.allure.Step("Click the\"Pogrzeby\" link")
     public void clickFuneralsLink() {
 
         clickLink(FUNERALS_SELECTOR);
     }
 
+    @io.qameta.allure.Step("Click the \"Szafarze\" link")
     public void clickStewardsLink() {
 
         clickLink(STEWARDS_SELECTOR);
     }
 
+    @io.qameta.allure.Step("Click the \"Kapłani\" link")
     public void clickPriestsLink() {
 
         clickLink(PRIESTS_SELECTOR);
     }
 
+    @io.qameta.allure.Step("Click the \"Spowiedź w bazylice\" link")
     public void clickConfessionLink() {
 
         clickLink(CONFESSION_SELECTOR);
     }
 
+    @io.qameta.allure.Step("Click the \"Kancelaria\" link")
     public void clickOfficeLink() {
 
         clickLink(OFFICE_SELECTOR);
