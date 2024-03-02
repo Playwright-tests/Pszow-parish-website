d7caea61a4e16d375a78e869cbf82d61cbee2c36
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-03-02 06:37:52 +0100

    Added fields

--- src/main/java/qa/utils/DataProviderNames.java
+++ src/main/java/qa/utils/DataProviderNames.java
@@ -4,4 +4,6 @@ public class DataProviderNames {
 
     public static final String CORRECT = "correct";
     public static final String INCORRECT = "incorrect";
+    public static final String EXPANDED = "expanded";
+    public static final String COLLAPSED = "collapsed";
 }
