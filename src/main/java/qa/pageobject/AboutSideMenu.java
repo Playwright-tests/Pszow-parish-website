e2957ffb98270dd070c3d65f4f663812b6957591
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-03-03 14:18:08 +0100

    Added the method

--- src/main/java/qa/pageobject/AboutSideMenu.java
+++ src/main/java/qa/pageobject/AboutSideMenu.java
@@ -10,6 +10,7 @@ public class AboutSideMenu extends Menu {
     private final String PILGRIMAGES_SELECTOR = "menu-item-24867";
     private final String PHOTO_GALLERY_SELECTOR = "menu-item-24862";
     private final String FILM_GALLERY_SELECTOR = "menu-item-24863";
+    private final String TRANSMISSION_SELECTOR = "menu-item-24861";
     private final String SUPPORT_SELECTOR = "menu-item-24860";
 
     public AboutSideMenu(Page page) {
@@ -18,42 +19,56 @@ public class AboutSideMenu extends Menu {
     }
 
     @io.qameta.allure.Step("Click the \"Kontakt\" link")
+    @io.qase.api.annotation.Step("Click the \"Kontakt\" link")
     public void clickContactLink() {
 
         clickLink(CONTACT_SELECTOR);
     }
 
     @io.qameta.allure.Step("Click the \"Historia parafii\" link")
+    @io.qase.api.annotation.Step("Click the \"Historia parafii\" link")
     public void clickParishHistoryLink() {
 
         clickLink(PARISH_HISTORY_SELECTOR);
     }
 
     @io.qameta.allure.Step("Click the \"Kalwaria Pszowska\" link")
+    @io.qase.api.annotation.Step("Click the \"Kalwaria Pszowska\" link")
     public void clickCalvaryLink() {
 
         clickLink(CALVARY_SELECTOR);
     }
 
-    @io.qameta.allure.Step("Click the \"Pilegrzymki\" link")
+    @io.qameta.allure.Step("Click the \"Pielgrzymki\" link")
+    @io.qase.api.annotation.Step("Click the \"Pielgrzymki\" link")
     public void clickPilgrimagesLink() {
 
         clickLink(PILGRIMAGES_SELECTOR);
     }
 
     @io.qameta.allure.Step("Click the \"Galeria zdjęć\" link")
+    @io.qase.api.annotation.Step("Click the \"Galeria zdjęć\" link")
     public void clickPhotoGalleryLink() {
 
         clickLink(PHOTO_GALLERY_SELECTOR);
     }
 
     @io.qameta.allure.Step("Click the \"Galeria filmów\" link")
+    @io.qase.api.annotation.Step("Click the \"Galeria filmów\" link")
     public void clickFilmGalleryLink() {
 
         clickLink(FILM_GALLERY_SELECTOR);
     }
 
+    @io.qameta.allure.Step("Click the \"Transmisja Mszy św.\" link")
+    @io.qase.api.annotation.Step("Click the \"Transmisja Mszy św.\" link")
+    public void clickTransmissionLink() {
+
+        clickLink(TRANSMISSION_SELECTOR);
+    }
+
     @io.qameta.allure.Step("Click the \"Wsparcie Parafii\" link")
+    @io.qase.api.annotation.Step("Click the \"Wsparcie Parafii\" link")
     public void clickSupportLink() {
 
         clickLink(SUPPORT_SELECTOR);
