5910dae38c6a3c9374c4f6ba26aace8957fc7b37
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-03-03 11:52:17 +0100

    Added the Allure and the Qase annotations

--- src/test/java/MediaIconsSectionTest.java
+++ src/test/java/MediaIconsSectionTest.java
@@ -1,10 +1,13 @@
 import com.microsoft.playwright.Page;
 import io.qameta.allure.*;
 
+import io.qase.api.annotation.QaseId;
+import io.qase.api.annotation.QaseTitle;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import qa.base.BaseTest;
+import qa.pageobject.Header;
 import qa.pageobject.MediaIconsSection;
 import qa.enums.URLs;
 
@@ -12,22 +15,22 @@ import java.util.function.Consumer;
 
 
 @Epic("E2E")
-@Feature("Media icons tests")
+@Feature("Media icons")
 public class MediaIconsSectionTest extends BaseTest {
 
-    private MediaIconsSection mediaIconsSection;
+    private Header header;
 
     @BeforeMethod
     public void create() {
 
         goToPage(URLs.BASE_URL.getName());
-        mediaIconsSection = new MediaIconsSection(getPage());
+        header = new Header(getPage());
     }
 
     private void actions(Consumer<MediaIconsSection> consumer, String url) {
 
         Page page = getPage().waitForPopup(() -> {
-            consumer.accept(mediaIconsSection);
+            consumer.accept(header.getMediaIconsSection());
         });
 
         Assert.assertTrue(page.url().contains(url));
@@ -35,6 +38,8 @@ public class MediaIconsSectionTest extends BaseTest {
 
     @Test
     @Description("The Facebook icon")
+    @QaseId(2)
+    @QaseTitle("The Facebook icon")
     public void facebookIcon() {
 
         actions(MediaIconsSection::clickFacebookIcon, URLs.FACEBOOK_PAGE.getName());
@@ -42,6 +47,8 @@ public class MediaIconsSectionTest extends BaseTest {
 
     @Test
     @Description("The Youtube icon")
+    @QaseId(3)
+    @QaseTitle("The Youtube icon")
     public void youtubeIcon() {
 
         actions(MediaIconsSection::clickYoutubeIcon, URLs.YOUTUBE_PAGE.getName());
