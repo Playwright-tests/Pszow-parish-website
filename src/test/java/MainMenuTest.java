5450509f2d277e934c263c5e5ecd002aab745929
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-03-03 12:23:49 +0100

    Added the Allure and the Qase annotations

--- src/test/java/MainMenuTest.java
+++ src/test/java/MainMenuTest.java
@@ -1,3 +1,5 @@
+import io.qase.api.annotation.QaseId;
+import io.qase.api.annotation.QaseTitle;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import qa.base.BaseTest;
@@ -30,50 +32,71 @@ public class MainMenuTest extends BaseTest {
                 "The page with address: " + url + " has not been opened");
     }
 
-    @Test
+    @Test(priority = 1)
+    @Severity(SeverityLevel.CRITICAL)
     @Description("The \"Ogłoszenia duszpasterskie\" link")
+    @QaseId(5)
+    @QaseTitle("The \"Ogłoszenia duszpasterskie\" link")
     public void announcementsLink() {
 
         actions(MainMenu::clickAnnouncementsLink, URLs.ANNOUNCEMENTS_PAGE.getName());
     }
 
-    @Test
+    @Test(priority = 2)
+    @Severity(SeverityLevel.CRITICAL)
     @Description("The \"Intencje mszalne\" link")
+    @QaseId(6)
+    @QaseTitle("The \"Intencje mszalne\" link")
     public void intentionsLink() {
 
         actions(MainMenu::clickIntentionsLink, URLs.INTENTION_PAGE.getName());
     }
 
-    @Test
+    @Test(priority = 3)
+    @Severity(SeverityLevel.CRITICAL)
     @Description("The \"Pogrzeby\" link")
+    @QaseId(7)
+    @QaseTitle("The \"Pogrzeby\" link")
     public void funeralsLink() {
 
         actions(MainMenu::clickFuneralsLink, URLs.FUNERALS_PAGE.getName());
     }
 
-    @Test
+    @Test(priority = 7)
+    @Severity(SeverityLevel.NORMAL)
     @Description("The \"Szafarze\" link")
+    @QaseId(8)
+    @QaseTitle("The \"Szafarze\" link")
     public void stewardsLink() {
 
         actions(MainMenu::clickStewardsLink, URLs.STEWARDS_PAGE.getName());
     }
 
-    @Test
+    @Test(priority = 6)
+    @Severity(SeverityLevel.NORMAL)
     @Description("The \"Duszpasterze\" link")
+    @QaseId(9)
+    @QaseTitle("The \"Duszpasterze\" link")
     public void priestsLink() {
 
         actions(MainMenu::clickPriestsLink, URLs.PRIESTS_PAGE.getName());
     }
 
-    @Test
+    @Test(priority = 5)
+    @Severity(SeverityLevel.CRITICAL)
     @Description("The \"Spowiedź\" link")
+    @QaseId(10)
+    @QaseTitle("The \"Spowiedź\" link")
     public void confessionLink() {
 
         actions(MainMenu::clickConfessionLink, URLs.CONFESSION_PAGE.getName());
     }
 
-    @Test
+    @Test(priority = 4)
+    @Severity(SeverityLevel.CRITICAL)
     @Description("The \"Kancelaria\" link")
+    @QaseId(11)
+    @QaseTitle("The \"Kancelaria\" link")
     public void officeLink() {
 
         actions(MainMenu::clickOfficeLink, URLs.OFFICE_PAGE.getName());
