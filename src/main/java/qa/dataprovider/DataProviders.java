7edc96ceec81be03d8489c0aca58a37ac08154cc
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-03-03 11:22:08 +0100

    Removed the method

--- src/main/java/qa/dataprovider/DataProviders.java
+++ src/main/java/qa/dataprovider/DataProviders.java
@@ -1,32 +1,26 @@
 package qa.dataprovider;
 
 import org.testng.annotations.DataProvider;
-import qa.jsonreader.JSONReader;
-import qa.utils.DataProviderNames;
+import qa.support.TestdataLoader;
+import qa.support.DataProviderNames;
 
 public class DataProviders {
 
-    @DataProvider(name = DataProviderNames.EXPANDED)
+    @DataProvider(name = DataProviderNames.ITEMS)
     public Object[] expanded() {
 
-        return JSONReader.getGalleryButtonNames(DataProviderNames.EXPANDED);
-    }
-
-    @DataProvider(name = DataProviderNames.COLLAPSED)
-    public Object[] collapsed() {
-
-        return JSONReader.getGalleryButtonNames(DataProviderNames.COLLAPSED);
+        return TestdataLoader.getItems(DataProviderNames.ITEMS);
     }
 
     @DataProvider(name = DataProviderNames.CORRECT)
     public Object[] correctPhrase() {
 
-        return JSONReader.getPhrases(DataProviderNames.CORRECT);
+        return TestdataLoader.getPhrases(DataProviderNames.CORRECT);
     }
 
     @DataProvider(name = DataProviderNames.INCORRECT)
     public Object[] incorrectPhrase() {
 
-        return  JSONReader.getPhrases(DataProviderNames.INCORRECT);
+        return  TestdataLoader.getPhrases(DataProviderNames.INCORRECT);
     }
 }
