843424d72b994833705c23862ce0087307c5d8e4
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-03-03 14:53:26 +0100

    Added the Allure an the Qase annotations

--- src/test/java/SacramentsSideMenuTest.java
+++ src/test/java/SacramentsSideMenuTest.java
@@ -1,4 +1,8 @@
 import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qase.api.annotation.QaseId;
+import io.qase.api.annotation.QaseTitle;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -28,29 +32,41 @@ public class SacramentsSideMenuTest extends BaseTest {
                 "The page with address: " + url + " has not been opened");
     }
 
-    @Test
+    @Test(priority = 1)
+    @Severity(SeverityLevel.CRITICAL)
     @Description("The \"Chrzest\" link")
+    @QaseId(20)
+    @QaseTitle("The \"Chrzest\" link")
     public void christeningLink() {
 
         actions(SacramentsSideMenu::clickChristeningLink, URLs.CHRISTENING_PAGE.getName());
     }
 
-    @Test
+    @Test(priority = 2)
+    @Severity(SeverityLevel.NORMAL)
     @Description("The \"Pierwsza Komunia\" link")
+    @QaseId(21)
+    @QaseTitle("The \"Pierwsza Komunia\" link")
     public void firstCommunionLink() {
 
         actions(SacramentsSideMenu::clickFirstCommunionLink, URLs.FIRST_COMMUNION_PAGE.getName());
     }
 
-    @Test
+    @Test(priority = 4)
+    @Severity(SeverityLevel.NORMAL)
     @Description("The \"Bierzmowanie\" link")
+    @QaseId(22)
+    @QaseTitle("The \"Bierzmowanie\" link")
     public void confirmationLink() {
 
         actions(SacramentsSideMenu::clickConfirmationLink, URLs.CONFIRMATION_PAGE.getName());
     }
 
-    @Test
+    @Test(priority = 3)
+    @Severity(SeverityLevel.NORMAL)
     @Description("The \"Małżeństwo\" link")
+    @QaseId(23)
+    @QaseTitle("The \"Małżeństwo\" link")
     public void marriageLink() {
 
         actions(SacramentsSideMenu::clickMarriageLink, URLs.MARRIAGE_PAGE.getName());
