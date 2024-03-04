b80ab114c0bba291bb29275b2605dec45ba625e6
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-03-04 08:34:05 +0100

    Added annotations

--- src/main/java/qa/pageobject/WorthSeeingSideMenu.java
+++ src/main/java/qa/pageobject/WorthSeeingSideMenu.java
@@ -16,30 +16,35 @@ public class WorthSeeingSideMenu extends Menu {
     }
 
     @io.qameta.allure.Step("Click the \"Litania do Pani Uśmiechniętej\" link")
+    @io.qase.api.annotation.Step("Click the \"Litania do Pani Uśmiechniętej\" link")
     public void clickLitanyLink() {
 
         clickLink(LITANY_SELECTOR);
     }
 
     @io.qameta.allure.Step("Click the \"Regulamin cmentarza\" link")
+    @io.qase.api.annotation.Step("Click the \"Regulamin cmentarza\" link")
     public void clickCementeryRegulationLink() {
 
         clickLink(CEMENTERY_REGULATION_SELECTOR);
     }
 
     @io.qameta.allure.Step("Click the \"Delegat ds. ochrony dzieci i młodzieży\" link")
+    @io.qase.api.annotation.Step("Click the \"Delegat ds. ochrony dzieci i młodzieży\" link")
     public void clickDelegateLink() {
 
         clickLink(DELEGATE_SELECTOR);
     }
 
     @io.qameta.allure.Step("Click the \"Archidiecezja Katowicka\" link")
+    @io.qase.api.annotation.Step("Click the \"Archidiecezja Katowicka\" link")
     public void clickArchdioceseLink() {
 
         clickLink(ARCHDIOCESE_SELECTOR);
     }
 
     @io.qameta.allure.Step("Click the \"Czytania liturgiczne\" link")
+    @io.qase.api.annotation.Step("Click the \"Czytania liturgiczne\" link")
     public void clickReadingLink() {
 
         clickLink(READING_SELECTOR);
