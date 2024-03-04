4729b0adb20671fdcef65dd5f7a5a0d1ee7fa1cd
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-03-04 07:59:01 +0100

    Added annotations

--- src/main/java/qa/pageobject/MinistrySideMenu.java
+++ src/main/java/qa/pageobject/MinistrySideMenu.java
@@ -22,66 +22,77 @@ public class MinistrySideMenu extends Menu {
     }
 
     @io.qameta.allure.Step("Click the \"Caritas i działalność charytatywna\" link")
+    @io.qase.api.annotation.Step("Click the \"Caritas i działalność charytatywna\" link")
     public void clickCaritasLink() {
 
         clickLink(CARITAS_SELECTOR);
     }
 
     @io.qameta.allure.Step("Click the \"Parafialna Rada Duszpasterska\" link")
+    @io.qase.api.annotation.Step("Click the \"Parafialna Rada Duszpasterska\" link")
     public void clickCouncilLink() {
 
         clickLink(COUNCIL_SELECTOR);
     }
 
     @io.qameta.allure.Step("Click the \"Ministranci i LSO\" link")
+    @io.qase.api.annotation.Step("Click the \"Ministranci i LSO\" link")
     public void clickAltarBoysLink() {
 
         clickLink(ALTAR_BOYS_SELECTOR);
     }
 
     @io.qameta.allure.Step("Click the \"Dzieci Maryi\" link")
+    @io.qase.api.annotation.Step("Click the \"Dzieci Maryi\" link")
     public void clickMarysChildrenLink() {
 
         clickLink(MARYS_CHILDREN_SELECTOR);
     }
 
     @io.qameta.allure.Step("Click the \"Oaza młodzieżowa\" link")
+    @io.qase.api.annotation.Step("Click the \"Oaza młodzieżowa\" link")
     public void clickOasisLink() {
 
         clickLink(OASIS_SELECTOR);
     }
 
     @io.qameta.allure.Step("Click the \"Domowy Kościół\" link")
+    @io.qase.api.annotation.Step("Click the \"Domowy Kościół\" link")
     public void clickHomeChurchLink() {
 
         clickLink(HOME_CHURCH_SELECTOR);
     }
 
     @io.qameta.allure.Step("Click the \"Katecheza dorosłych\" link")
+    @io.qase.api.annotation.Step("Click the \"Katecheza dorosłych\" link")
     public void clickCatechesisLink() {
 
         clickLink(CATECHESIS_SELECTOR);
     }
 
     @io.qameta.allure.Step("Click the \"PoCo\" link")
+    @io.qase.api.annotation.Step("Click the \"PoCo\" link")
     public void clickPocoLink() {
 
         clickLink(POCO_SELECTOR);
     }
 
     @io.qameta.allure.Step("Click the \"Legion Maryi\" link")
+    @io.qase.api.annotation.Step("Click the \"Legion Maryi\" link")
     public void clickLegionOfMaryLink() {
 
         clickLink(LEGION_OF_MARY_SELECTOR);
     }
 
     @io.qameta.allure.Step("Click the \"Żywy Różaniec\" link")
+    @io.qase.api.annotation.Step("Click the \"Żywy Różaniec\" link")
     public void clickLivingRosaryLink() {
 
         clickLink(LIVING_ROSARY_SELECTOR);
     }
 
     @io.qameta.allure.Step("Click the \"Wspólnota Matki Miłosiernego\" link")
+    @io.qase.api.annotation.Step("Click the \"Wspólnota Matki Miłosiernego\" link")
     public void clickMothersCommunityLink() {
 
         clickLink(MOTHERS_COMMUNITY_SELECTOR);
--- src/test/java/MinistrySideMenuTest.java
+++ src/test/java/MinistrySideMenuTest.java
@@ -1,4 +1,6 @@
-import io.qameta.allure.Description;
+import io.qameta.allure.*;
+import io.qase.api.annotation.QaseId;
+import io.qase.api.annotation.QaseTitle;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -8,6 +10,8 @@ import qa.pageobject.MinistrySideMenu;
 
 import java.util.function.Consumer;
 
+@Epic("E2E")
+@Feature("The \"Duszpasterstwo\" side menu links")
 public class MinistrySideMenuTest extends BaseTest {
 
     private MinistrySideMenu ministrySideMenu;
@@ -27,78 +31,111 @@ public class MinistrySideMenuTest extends BaseTest {
                 "The page with address: " + url + " has not been opened");
     }
 
-    @Test
+    @Test(priority = 11)
+    @Severity(SeverityLevel.NORMAL)
     @Description("The \"Caritas i działalność charytatywna\" link")
+    @QaseId(24)
+    @QaseTitle("The \"Caritas i działalność charytatywna\" link")
     public void caritasLink() {
 
         actions(MinistrySideMenu::clickCaritasLink, URLs.CARITAS.getName());
     }
 
-    @Test
+    @Test(priority = 6)
+    @Severity(SeverityLevel.MINOR)
     @Description("The \"Parafialna Rada Duszpasterska\" link")
+    @QaseId(25)
+    @QaseTitle("The \"Parafialna Rada Duszpasterska\" link")
     public void councilLink() {
 
         actions(MinistrySideMenu::clickCouncilLink, URLs.COUNCIL_PAGE.getName());
     }
 
-    @Test
+    @Test(priority = 7)
+    @Severity(SeverityLevel.MINOR)
     @Description("The \"Ministranci i LSO\" link")
+    @QaseId(26)
+    @QaseTitle("The \"Ministranci i LSO\" link")
     public void altarBoysLink() {
 
         actions(MinistrySideMenu::clickAltarBoysLink, URLs.ALTAR_BOYS_PAGE.getName());
     }
 
-    @Test
+    @Test(priority = 10)
+    @Severity(SeverityLevel.MINOR)
     @Description("The \"Dzieci Maryi\" link")
+    @QaseId(27)
+    @QaseTitle("The \"Dzieci Maryi\" link")
     public void marysChildrenLink() {
 
         actions(MinistrySideMenu::clickMarysChildrenLink, URLs.MARYS_CHILDREN_PAGE.getName());
     }
 
-    @Test
+    @Test(priority = 8)
+    @Severity(SeverityLevel.MINOR)
     @Description("The \"Oaza młodzieżowa\" link")
+    @QaseId(28)
+    @QaseTitle("The \"Oaza młodzieżowa\" link")
     public void oasisLink() {
 
         actions(MinistrySideMenu::clickOasisLink, URLs.OASIS_PAGE.getName());
     }
 
-    @Test
+    @Test(priority = 9)
+    @Severity(SeverityLevel.MINOR)
     @Description("The \"Domowy Kościół\" link")
+    @QaseId(29)
+    @QaseTitle("The \"Domowy Kościół\" link")
     public void homeChurchLink() {
 
         actions(MinistrySideMenu::clickHomeChurchLink, URLs.HOME_CHURCH_PAGE.getName());
     }
 
-    @Test
+    @Test(priority = 1)
+    @Severity(SeverityLevel.MINOR)
     @Description("The \"Katecheza dorosłych\" link")
+    @QaseId(30)
+    @QaseTitle("The \"Katecheza dorosłych\" link")
     public void catechesisLink() {
 
         actions(MinistrySideMenu::clickCatechesisLink, URLs.CATECHESIS_PAGE.getName());
     }
 
-    @Test
+    @Test(priority = 5)
+    @Severity(SeverityLevel.MINOR)
     @Description("The \"PoCo\" link")
+    @QaseId(31)
+    @QaseTitle("The \"PoCo\" link")
     public void pocoLink() {
 
         actions(MinistrySideMenu::clickPocoLink, URLs.POCO_PAGE.getName());
     }
 
-    @Test
+    @Test(priority = 3)
+    @Severity(SeverityLevel.NORMAL)
     @Description("The \"Legion Maryi\" link")
+    @QaseId(32)
+    @QaseTitle("The \"Legion Maryi\" link")
     public void legionOfMaryLink() {
 
         actions(MinistrySideMenu::clickLegionOfMaryLink, URLs.LEGION_OF_MARY.getName());
     }
 
-    @Test
+    @Test(priority = 2)
+    @Severity(SeverityLevel.NORMAL)
     @Description("The \"Żywy Różaniec\" link")
+    @QaseId(33)
+    @QaseTitle("The \"Żywy Różaniec\" link")
     public void livingRosaryLink() {
 
         actions(MinistrySideMenu::clickLivingRosaryLink, URLs.LIVING_ROSARY_PAGE.getName());
     }
 
-    @Test
+    @Test(priority = 4)
+    @Severity(SeverityLevel.NORMAL)
     @Description("The \"Wspólnota Matki Miłosiernego\" link")
+    @QaseId(34)
+    @QaseTitle("The \"Wspólnota Matki Miłosiernego\" link")
     public void mothersCommunityLink() {
 
         actions(MinistrySideMenu::clickMothersCommunityLink, URLs.MOTHERS_COMMUNITY.getName());
