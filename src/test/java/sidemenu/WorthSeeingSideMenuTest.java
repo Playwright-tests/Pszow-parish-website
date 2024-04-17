7aa210d0e671aa8ae60080e83a0eb810a887787e
pawelaksman <pablo_xmen@op.pl>
2024-04-17 23:11:27 +0200

    Added annotations

--- src/test/java/sidemenu/WorthSeeingSideMenuTest.java
+++ src/test/java/sidemenu/WorthSeeingSideMenuTest.java
@@ -21,7 +21,7 @@ public class WorthSeeingSideMenuTest extends BaseTest {
     @BeforeMethod
     public void create() {
 
-        goToPage(URLs.BASE_URL.getName());
+        goToPage(URLs.HOME_PAGE);
         worthSeeingSideMenu = new WorthSeeingSideMenu(getPage());
     }
 
@@ -33,53 +33,33 @@ public class WorthSeeingSideMenuTest extends BaseTest {
                 "The page with address: " + url + " has not been opened");
     }
 
-    @Test(priority = 4)
+    @Test(priority = 1)
     @Severity(SeverityLevel.NORMAL)
     @Description("The \"Litania do Pani Uśmiechniętej\" link")
     @QaseId(35)
     @QaseTitle("The \"Litania do Pani Uśmiechniętej\" link")
     public void litanyLink() {
 
-        actions(WorthSeeingSideMenu::clickLitanyLink, URLs.LITANY_PAGE.getName());
+        actions(WorthSeeingSideMenu::clickLitanyLink, URLs.LITANY_PAGE);
     }
 
-    @Test(priority = 1)
+    @Test(priority = 2)
     @Severity(SeverityLevel.CRITICAL)
     @Description("The \"Regulamin cmentarza\" link")
     @QaseId(36)
     @QaseTitle("The \"Regulamin cmentarza\" link")
-    public void cementeryRegulationLink() {
+    public void cemeteryRegulationLink() {
 
-        actions(WorthSeeingSideMenu::clickCementeryRegulationLink, URLs.CEMENTERY_REGULATION.getName());
+        actions(WorthSeeingSideMenu::clickCementeryRegulationLink, URLs.CEMENTERY_REGULATION);
     }
 
-    @Test(priority = 5)
+    @Test(priority = 3)
     @Severity(SeverityLevel.MINOR)
     @Description("The \"Delegat ds. ochrony dzieci i młodzieży\" link")
     @QaseId(37)
     @QaseTitle("The \"Delegat ds. ochrony dzieci i młodzieży\" link")
     public void delegateLink() {
 
-        actions(WorthSeeingSideMenu::clickDelegateLink, URLs.DELEGATE_PAGE.getName());
-    }
-
-    @Test(priority = 2)
-    @Severity(SeverityLevel.CRITICAL)
-    @Description("The \"Archidiecezja Katowicka\" link")
-    @QaseId(38)
-    @QaseTitle("The \"Archidiecezja Katowicka\" link")
-    public void archdioceseLink() {
-
-        actions(WorthSeeingSideMenu::clickArchdioceseLink, URLs.ARCHDIOCESE.getName());
-    }
-
-    @Test(priority = 3)
-    @Severity(SeverityLevel.CRITICAL)
-    @Description("The \"Czytania liturgiczne\" link")
-    @QaseId(39)
-    @QaseTitle("The \"Czytania liturgiczne\" link")
-    public void readingLink() {
-
-        actions(WorthSeeingSideMenu::clickReadingLink, URLs.READING.getName());
+        actions(WorthSeeingSideMenu::clickDelegateLink, URLs.DELEGATE_PAGE);
     }
 }
