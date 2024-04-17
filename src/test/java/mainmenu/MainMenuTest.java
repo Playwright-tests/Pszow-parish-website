ff6c32bc2551777b3ccf844cb3efab69e1c2100f
pawelaksman <pablo_xmen@op.pl>
2024-04-17 22:46:06 +0200

    Added annotations

--- src/test/java/mainmenu/MainMenuTest.java
+++ src/test/java/mainmenu/MainMenuTest.java
@@ -2,6 +2,7 @@ package mainmenu;
 
 import io.qase.api.annotation.QaseId;
 import io.qase.api.annotation.QaseTitle;
+import org.junit.jupiter.api.Tag;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import qa.base.BaseTest;
@@ -9,6 +10,7 @@ import org.testng.annotations.Test;
 import qa.enums.URLs;
 import io.qameta.allure.*;
 import qa.pageobject.MainMenu;
+import qa.support.AllureAttachments;
 
 import java.util.function.Consumer;
 
@@ -21,7 +23,7 @@ public class MainMenuTest extends BaseTest {
     @BeforeMethod
     public void create() {
 
-        goToPage(URLs.BASE_URL.getName());
+        goToPage(URLs.HOME_PAGE);
 
         mainMenu = new MainMenu(getPage());
     }
@@ -30,77 +32,106 @@ public class MainMenuTest extends BaseTest {
 
         consumer.accept(mainMenu);
 
+
         Assert.assertEquals(getPage().url(), url,
                 "The page with address: " + url + " has not been opened");
     }
 
     @Test(priority = 1)
+    @Tag("Links")
+    @Tag("Main menu")
+    @Owner("Paweł Aksman")
     @Severity(SeverityLevel.CRITICAL)
     @Description("The \"Ogłoszenia duszpasterskie\" link")
     @QaseId(5)
     @QaseTitle("The \"Ogłoszenia duszpasterskie\" link")
     public void announcementsLink() {
 
-        actions(MainMenu::clickAnnouncementsLink, URLs.ANNOUNCEMENTS_PAGE.getName());
+        AllureAttachments.takeScreenshot(mainMenu.getAnnouncementLink(), "announcementLink");
+        actions(MainMenu::clickAnnouncementsLink, URLs.ANNOUNCEMENTS_PAGE);
     }
 
     @Test(priority = 2)
+    @Tag("Links")
+    @Tag("Main menu")
+    @Owner("Paweł Aksman")
     @Severity(SeverityLevel.CRITICAL)
     @Description("The \"Intencje mszalne\" link")
     @QaseId(6)
     @QaseTitle("The \"Intencje mszalne\" link")
     public void intentionsLink() {
 
-        actions(MainMenu::clickIntentionsLink, URLs.INTENTION_PAGE.getName());
+        AllureAttachments.takeScreenshot(mainMenu.getIntentionsLink(), "intentionsLink");
+        actions(MainMenu::clickIntentionsLink, URLs.INTENTION_PAGE);
     }
 
     @Test(priority = 3)
+    @Tag("Links")
+    @Tag("Main menu")
+    @Owner("Paweł Aksman")
     @Severity(SeverityLevel.CRITICAL)
     @Description("The \"Pogrzeby\" link")
     @QaseId(7)
     @QaseTitle("The \"Pogrzeby\" link")
     public void funeralsLink() {
 
-        actions(MainMenu::clickFuneralsLink, URLs.FUNERALS_PAGE.getName());
+        AllureAttachments.takeScreenshot(mainMenu.getFuneralsLink(), "funeralsLink");
+        actions(MainMenu::clickFuneralsLink, URLs.FUNERALS_PAGE);
     }
 
     @Test(priority = 7)
-    @Severity(SeverityLevel.NORMAL)
+    @Tag("Links")
+    @Tag("Main menu")
+    @Owner("Paweł Aksman")
+    @Severity(SeverityLevel.MINOR)
     @Description("The \"Szafarze\" link")
     @QaseId(8)
     @QaseTitle("The \"Szafarze\" link")
     public void stewardsLink() {
 
-        actions(MainMenu::clickStewardsLink, URLs.STEWARDS_PAGE.getName());
+        AllureAttachments.takeScreenshot(mainMenu.getStewardsLink(), "stewardsLink");
+        actions(MainMenu::clickStewardsLink, URLs.STEWARDS_PAGE);
     }
 
     @Test(priority = 6)
-    @Severity(SeverityLevel.NORMAL)
+    @Tag("Links")
+    @Tag("Main menu")
+    @Owner("Paweł Aksman")
+    @Severity(SeverityLevel.CRITICAL)
     @Description("The \"Duszpasterze\" link")
     @QaseId(9)
     @QaseTitle("The \"Duszpasterze\" link")
     public void priestsLink() {
 
-        actions(MainMenu::clickPriestsLink, URLs.PRIESTS_PAGE.getName());
+        AllureAttachments.takeScreenshot(mainMenu.getPriestsLink(), "priestsLink");
+        actions(MainMenu::clickPriestsLink, URLs.PRIESTS_PAGE);
     }
 
     @Test(priority = 5)
+    @Tag("Links")
+    @Tag("Main menu")
+    @Owner("Paweł Aksman")
     @Severity(SeverityLevel.CRITICAL)
     @Description("The \"Spowiedź\" link")
     @QaseId(10)
     @QaseTitle("The \"Spowiedź\" link")
     public void confessionLink() {
 
-        actions(MainMenu::clickConfessionLink, URLs.CONFESSION_PAGE.getName());
+        AllureAttachments.takeScreenshot(mainMenu.getConfessionLink(), "confessionLink");
+        actions(MainMenu::clickConfessionLink, URLs.CONFESSION_PAGE);
     }
 
     @Test(priority = 4)
+    @Tag("Links")
+    @Tag("Main menu")
+    @Owner("Paweł Aksman")
     @Severity(SeverityLevel.CRITICAL)
     @Description("The \"Kancelaria\" link")
     @QaseId(11)
     @QaseTitle("The \"Kancelaria\" link")
     public void officeLink() {
 
-        actions(MainMenu::clickOfficeLink, URLs.OFFICE_PAGE.getName());
+        AllureAttachments.takeScreenshot(mainMenu.getOfficeLink(), "officeLink");
+        actions(MainMenu::clickOfficeLink, URLs.OFFICE_PAGE);
     }
 }
