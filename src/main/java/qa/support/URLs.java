3cf6409223c8f9de9249edae3d62ec6235c29e54
pawelaksman <pablo_xmen@op.pl>
2024-04-17 23:23:37 +0200

    Created

---
+++ src/main/java/qa/support/URLs.java
@@ -0,0 +1,44 @@
+package qa.support;
+
+public class URLs {
+
+    public static final String HOME_PAGE = "https://bazylika-pszow.pl/";
+    public static final String ANNOUNCEMENTS_PAGE = "https://bazylika-pszow.pl/ogloszenia-duszpasterskie/";
+    public static final String INTENTION_PAGE = "https://bazylika-pszow.pl/intencje-mszalne/";
+    public static final String FUNERALS_PAGE = "https://bazylika-pszow.pl/pogrzeby/";
+    public static final String STEWARDS_PAGE = "https://bazylika-pszow.pl/szafarze/";
+    public static final String PRIESTS_PAGE = "https://bazylika-pszow.pl/kaplani/";
+    public static final String CONFESSION_PAGE = "https://bazylika-pszow.pl/spowiedz-w-bazylice/";
+    public static final String OFFICE_PAGE = "https://bazylika-pszow.pl/kancelaria/";
+    public static final String PHOTO_GALLERY = "https://bazylika-pszow.pl/galeria-zdjec/";
+    public static final String CONTACT_PAGE = "https://bazylika-pszow.pl/kontakt/";
+    public static final String HISTORY_PAGE = "https://bazylika-pszow.pl/historia-parafii/";
+    public static final String CALVARY_PAGE = "https://bazylika-pszow.pl/kalwaria-pszowska/";
+    public static final String PILGRIMAGES_PAGE = "https://bazylika-pszow.pl/pielgrzymki/";
+    public static final String FILM_GALLERY_PAGE = "https://bazylika-pszow.pl/galeria-filmow/";
+    public static final String SUPPORT_PAGE = "https://bazylika-pszow.pl/wsparcie-parafii/";
+    public static final String CHRISTENING_PAGE = "https://bazylika-pszow.pl/sakramenty/";
+    public static final String FIRST_COMMUNION_PAGE = "https://bazylika-pszow.pl/pierwsza-komunia/";
+    public static final String CONFIRMATION_PAGE = "https://bazylika-pszow.pl/sakrament-bierzmowania/";
+    public static final String MARRIAGE_PAGE = "https://bazylika-pszow.pl/sakrament-malzenstwa/";
+    public static final String CARITAS = "https://bazylika-pszow.pl/caritas-i-dzialalnosc-charytatywna/";
+    public static final String COUNCIL_PAGE = "https://bazylika-pszow.pl/parafialna-rada-duszpasterska/";
+    public static final String ALTAR_BOYS_PAGE = "https://bazylika-pszow.pl/ministranci/";
+    public static final String MARYS_CHILDREN_PAGE = "https://bazylika-pszow.pl/dzieci-maryi-2/";
+    public static final String OASIS_PAGE = "https://bazylika-pszow.pl/oaza-mlodziezowa/";
+    public static final String HOME_CHURCH_PAGE = "https://bazylika-pszow.pl/ruch-domowy-kosciol/";
+    public static final String CATECHESIS_PAGE = "https://bazylika-pszow.pl/katecheza-doroslych/";
+    public static final String POCO_PAGE = "https://bazylika-pszow.pl/grupa-poco/";
+    public static final String LEGION_OF_MARY = "https://bazylika-pszow.pl/legion-maryi/";
+    public static final String LIVING_ROSARY_PAGE = "https://bazylika-pszow.pl/bractwo-zywego-rozanca/";
+    public static final String MOTHERS_COMMUNITY = "https://bazylika-pszow.pl/wspolnota-matki-milosiernego/";
+    public static final String LITANY_PAGE = "https://bazylika-pszow.pl/litania-do-pani-usmiechnietej/";
+    public static final String CEMENTERY_REGULATION = "https://bazylika-pszow.pl/regulamin-cmentarza/";
+    public static final String DELEGATE_PAGE = "https://bazylika-pszow.pl/delegat-ds-ochrony-dzieci-i-mlodziezy/";
+    public static final String ARCHDIOCESE = "https://www.archidiecezjakatowicka.pl/";
+    public static final String READING = "https://www.paulus.org.pl/czytania";
+    public static final String FACEBOOK_PAGE = "https://www.facebook.com";
+    public static final String YOUTUBE_PAGE = "https://consent.youtube.com/";
+    public static final String YOUTUBE_TRANSMISSION = "https://www.youtube.com/watch";
+    public static final String NEWS_PAGE = "https://bazylika-pszow.pl/category/aktualnosci/";
+}
