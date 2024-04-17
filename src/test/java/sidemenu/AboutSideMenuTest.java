48471db06e2950e5233131dca2ed29a2d79698de
pawelaksman <pablo_xmen@op.pl>
2024-04-17 22:58:13 +0200

    Added annotations

--- src/test/java/sidemenu/AboutSideMenuTest.java
+++ src/test/java/sidemenu/AboutSideMenuTest.java
@@ -3,6 +3,7 @@ package sidemenu;
 import io.qameta.allure.*;
 import io.qase.api.annotation.QaseId;
 import io.qase.api.annotation.QaseTitle;
+import org.junit.jupiter.api.Tag;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -22,7 +23,7 @@ public class AboutSideMenuTest extends BaseTest {
     @BeforeMethod
     public void create() {
 
-        goToPage(URLs.BASE_URL.getName());
+        goToPage(URLs.HOME_PAGE);
 
         aboutSideMenu = new AboutSideMenu(getPage());
     }
@@ -35,83 +36,97 @@ public class AboutSideMenuTest extends BaseTest {
                 "The page with address: " + url + " has not been opened");
     }
 
-    @Test(priority = 3)
+    @Test(priority = 1)
+    @Tag("Links")
+    @Tag("Side menu")
+    @Owner("Paweł Aksman")
+    @Link(name = "Home page", url = URLs.HOME_PAGE)
     @Severity(SeverityLevel.CRITICAL)
     @Description("The \"Kontakt\" link")
     @QaseId(12)
     @QaseTitle("The \"Kontakt\" link")
     public void contactLink() {
 
-        actions(AboutSideMenu::clickContactLink, URLs.CONTACT_PAGE.getName());
+        actions(AboutSideMenu::clickContactLink, URLs.CONTACT_PAGE);
     }
 
-    @Test(priority = 6)
-    @Severity(SeverityLevel.NORMAL)
+    @Test(priority = 4)
+    @Tag("Links")
+    @Tag("Side menu")
+    @Owner("Paweł Aksman")
+    @Link(name = "Home page", url = URLs.HOME_PAGE)
+    @Severity(SeverityLevel.CRITICAL)
     @Description("The \"Historia parafii\" link")
     @QaseId(13)
     @QaseTitle("The \"Historia parafii\" link")
     public void historyLink() {
 
-        actions(AboutSideMenu::clickParishHistoryLink, URLs.HISTORY_PAGE.getName());
+        actions(AboutSideMenu::clickParishHistoryLink, URLs.HISTORY_PAGE);
     }
 
-    @Test(priority = 7)
-    @Severity(SeverityLevel.NORMAL)
+    @Test(priority = 6)
+    @Severity(SeverityLevel.MINOR)
     @Description("The \"Kalwaria Pszowska\" link")
     @QaseId(14)
     @QaseTitle("The \"Kalwaria Pszowska\" link")
     public void calvaryLink() {
 
-        actions(AboutSideMenu::clickCalvaryLink, URLs.CALVARY_PAGE.getName());
+        actions(AboutSideMenu::clickCalvaryLink, URLs.CALVARY_PAGE);
     }
 
-    @Test(priority = 8)
-    @Severity(SeverityLevel.NORMAL)
+    @Test(priority = 7)
+    @Tag("Links")
+    @Tag("Side menu")
+    @Owner("Paweł Aksman")
+    @Link(name = "Home page", url = URLs.HOME_PAGE)
+    @Severity(SeverityLevel.MINOR)
     @Description("The \"Pielgrzymki\" link")
     @QaseId(15)
     @QaseTitle("The \"Pielgrzymki\" link")
     public void pilgrimagesLink() {
 
-        actions(AboutSideMenu::clickPilgrimagesLink, URLs.PILGRIMAGES_PAGE.getName());
+        actions(AboutSideMenu::clickPilgrimagesLink, URLs.PILGRIMAGES_PAGE);
     }
 
     @Test(priority = 2)
+    @Tag("Links")
+    @Tag("Side menu")
+    @Owner("Paweł Aksman")
+    @Link(name = "Home page", url = URLs.HOME_PAGE)
     @Severity(SeverityLevel.CRITICAL)
     @Description("The \"Galeria zdjęć\" link")
     @QaseId(16)
     @QaseTitle("The \"Galeria zdjęć\" link")
     public void photoGalleryLink() {
 
-        actions(AboutSideMenu::clickPhotoGalleryLink, URLs.PHOTO_GALLERY.getName());
+        actions(AboutSideMenu::clickPhotoGalleryLink, URLs.PHOTO_GALLERY);
     }
 
-    @Test(priority = 4)
+    @Test(priority = 3)
+    @Tag("Links")
+    @Tag("Side menu")
+    @Owner("Paweł Aksman")
+    @Link(name = "Home page", url = URLs.HOME_PAGE)
     @Severity(SeverityLevel.CRITICAL)
     @Description("The \"Galeria filmów\" link")
     @QaseId(17)
     @QaseTitle("The \"Galeria filmów\" link")
     public void filmGalleryLink() {
 
-        actions(AboutSideMenu::clickFilmGalleryLink, URLs.FILM_GALLERY_PAGE.getName());
-    }
-
-    @Test(priority = 1)
-    @Severity(SeverityLevel.CRITICAL)
-    @Description("The \"Transmisja Mszy św.\" link")
-    @QaseId(18)
-    @QaseTitle("The \"Transmisja Mszy św.\" link")
-    public void transmissionLink() {
-
-        actions(AboutSideMenu::clickTransmissionLink, URLs.YOUTUBE_TRANSMISSION.getName());
+        actions(AboutSideMenu::clickFilmGalleryLink, URLs.FILM_GALLERY_PAGE);
     }
 
     @Test(priority = 5)
-    @Severity(SeverityLevel.NORMAL)
+    @Tag("Links")
+    @Tag("Side menu")
+    @Owner("Paweł Aksman")
+    @Link(name = "Home page", url = URLs.HOME_PAGE)
+    @Severity(SeverityLevel.CRITICAL)
     @Description("The \"Wsparcie parafii\" link")
     @QaseId(19)
     @QaseTitle("The \"Wsparcie parafii\" link")
     public void supportLink() {
 
-        actions(AboutSideMenu::clickSupportLink, URLs.SUPPORT_PAGE.getName());
+        actions(AboutSideMenu::clickSupportLink, URLs.SUPPORT_PAGE);
     }
 }
