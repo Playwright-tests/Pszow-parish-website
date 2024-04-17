f12dfc5bd494981cec063a896e5183eb37b800ed
pawelaksman <pablo_xmen@op.pl>
2024-04-17 23:10:12 +0200

    Added annotations

--- src/test/java/sidemenu/MinistrySideMenuTest.java
+++ src/test/java/sidemenu/MinistrySideMenuTest.java
@@ -3,6 +3,7 @@ package sidemenu;
 import io.qameta.allure.*;
 import io.qase.api.annotation.QaseId;
 import io.qase.api.annotation.QaseTitle;
+import org.junit.jupiter.api.Tag;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -21,7 +22,7 @@ public class MinistrySideMenuTest extends BaseTest {
     @BeforeMethod
     public void create() {
 
-        goToPage(URLs.BASE_URL.getName());
+        goToPage(URLs.HOME_PAGE);
         ministrySideMenu = new MinistrySideMenu(getPage());
     }
 
@@ -33,113 +34,157 @@ public class MinistrySideMenuTest extends BaseTest {
                 "The page with address: " + url + " has not been opened");
     }
 
-    @Test(priority = 11)
+    @Test(priority = 3)
+    @Tag("Links")
+    @Tag("Side menu")
+    @Owner("Paweł Aksman")
+    @Link(name = "Home page", url = URLs.HOME_PAGE)
     @Severity(SeverityLevel.NORMAL)
     @Description("The \"Caritas i działalność charytatywna\" link")
     @QaseId(24)
     @QaseTitle("The \"Caritas i działalność charytatywna\" link")
     public void caritasLink() {
 
-        actions(MinistrySideMenu::clickCaritasLink, URLs.CARITAS.getName());
+        actions(MinistrySideMenu::clickCaritasLink, URLs.CARITAS);
     }
 
-    @Test(priority = 6)
+    @Test(priority = 4)
+    @Tag("Links")
+    @Tag("Side menu")
+    @Owner("Paweł Aksman")
+    @Link(name = "Home page", url = URLs.HOME_PAGE)
     @Severity(SeverityLevel.MINOR)
     @Description("The \"Parafialna Rada Duszpasterska\" link")
     @QaseId(25)
     @QaseTitle("The \"Parafialna Rada Duszpasterska\" link")
     public void councilLink() {
 
-        actions(MinistrySideMenu::clickCouncilLink, URLs.COUNCIL_PAGE.getName());
+        actions(MinistrySideMenu::clickCouncilLink, URLs.COUNCIL_PAGE);
     }
 
     @Test(priority = 7)
+    @Tag("Links")
+    @Tag("Side menu")
+    @Owner("Paweł Aksman")
+    @Link(name = "Home page", url = URLs.HOME_PAGE)
     @Severity(SeverityLevel.MINOR)
     @Description("The \"Ministranci i LSO\" link")
     @QaseId(26)
     @QaseTitle("The \"Ministranci i LSO\" link")
     public void altarBoysLink() {
 
-        actions(MinistrySideMenu::clickAltarBoysLink, URLs.ALTAR_BOYS_PAGE.getName());
+        actions(MinistrySideMenu::clickAltarBoysLink, URLs.ALTAR_BOYS_PAGE);
     }
 
-    @Test(priority = 10)
+    @Test(priority = 6)
+    @Tag("Links")
+    @Tag("Side menu")
+    @Owner("Paweł Aksman")
+    @Link(name = "Home page", url = URLs.HOME_PAGE)
     @Severity(SeverityLevel.MINOR)
     @Description("The \"Dzieci Maryi\" link")
     @QaseId(27)
     @QaseTitle("The \"Dzieci Maryi\" link")
     public void marysChildrenLink() {
 
-        actions(MinistrySideMenu::clickMarysChildrenLink, URLs.MARYS_CHILDREN_PAGE.getName());
+        actions(MinistrySideMenu::clickMarysChildrenLink, URLs.MARYS_CHILDREN_PAGE);
     }
 
-    @Test(priority = 8)
+    @Test(priority = 5)
+    @Tag("Links")
+    @Tag("Side menu")
+    @Owner("Paweł Aksman")
+    @Link(name = "Home page", url = URLs.HOME_PAGE)
     @Severity(SeverityLevel.MINOR)
     @Description("The \"Oaza młodzieżowa\" link")
     @QaseId(28)
     @QaseTitle("The \"Oaza młodzieżowa\" link")
     public void oasisLink() {
 
-        actions(MinistrySideMenu::clickOasisLink, URLs.OASIS_PAGE.getName());
+        actions(MinistrySideMenu::clickOasisLink, URLs.OASIS_PAGE);
     }
 
     @Test(priority = 9)
+    @Tag("Links")
+    @Tag("Side menu")
+    @Owner("Paweł Aksman")
+    @Link(name = "Home page", url = URLs.HOME_PAGE)
     @Severity(SeverityLevel.MINOR)
     @Description("The \"Domowy Kościół\" link")
     @QaseId(29)
     @QaseTitle("The \"Domowy Kościół\" link")
     public void homeChurchLink() {
 
-        actions(MinistrySideMenu::clickHomeChurchLink, URLs.HOME_CHURCH_PAGE.getName());
+        actions(MinistrySideMenu::clickHomeChurchLink, URLs.HOME_CHURCH_PAGE);
     }
 
-    @Test(priority = 1)
+    @Test(priority = 10)
+    @Tag("Links")
+    @Tag("Side menu")
+    @Owner("Paweł Aksman")
+    @Link(name = "Home page", url = URLs.HOME_PAGE)
     @Severity(SeverityLevel.MINOR)
     @Description("The \"Katecheza dorosłych\" link")
     @QaseId(30)
     @QaseTitle("The \"Katecheza dorosłych\" link")
     public void catechesisLink() {
 
-        actions(MinistrySideMenu::clickCatechesisLink, URLs.CATECHESIS_PAGE.getName());
+        actions(MinistrySideMenu::clickCatechesisLink, URLs.CATECHESIS_PAGE);
     }
 
-    @Test(priority = 5)
+    @Test(priority = 11)
+    @Tag("Links")
+    @Tag("Side menu")
+    @Owner("Paweł Aksman")
+    @Link(name = "Home page", url = URLs.HOME_PAGE)
     @Severity(SeverityLevel.MINOR)
     @Description("The \"PoCo\" link")
     @QaseId(31)
     @QaseTitle("The \"PoCo\" link")
     public void pocoLink() {
 
-        actions(MinistrySideMenu::clickPocoLink, URLs.POCO_PAGE.getName());
+        actions(MinistrySideMenu::clickPocoLink, URLs.POCO_PAGE);
     }
 
-    @Test(priority = 3)
-    @Severity(SeverityLevel.NORMAL)
+    @Test(priority = 1)
+    @Tag("Links")
+    @Tag("Side menu")
+    @Owner("Paweł Aksman")
+    @Link(name = "Home page", url = URLs.HOME_PAGE)
+    @Severity(SeverityLevel.CRITICAL)
     @Description("The \"Legion Maryi\" link")
     @QaseId(32)
     @QaseTitle("The \"Legion Maryi\" link")
     public void legionOfMaryLink() {
 
-        actions(MinistrySideMenu::clickLegionOfMaryLink, URLs.LEGION_OF_MARY.getName());
+        actions(MinistrySideMenu::clickLegionOfMaryLink, URLs.LEGION_OF_MARY);
     }
 
     @Test(priority = 2)
-    @Severity(SeverityLevel.NORMAL)
+    @Tag("Links")
+    @Tag("Side menu")
+    @Owner("Paweł Aksman")
+    @Link(name = "Home page", url = URLs.HOME_PAGE)
+    @Severity(SeverityLevel.CRITICAL)
     @Description("The \"Żywy Różaniec\" link")
     @QaseId(33)
     @QaseTitle("The \"Żywy Różaniec\" link")
     public void livingRosaryLink() {
 
-        actions(MinistrySideMenu::clickLivingRosaryLink, URLs.LIVING_ROSARY_PAGE.getName());
+        actions(MinistrySideMenu::clickLivingRosaryLink, URLs.LIVING_ROSARY_PAGE);
     }
 
-    @Test(priority = 4)
+    @Test(priority = 8)
+    @Tag("Links")
+    @Tag("Side menu")
+    @Owner("Paweł Aksman")
+    @Link(name = "Home page", url = URLs.HOME_PAGE)
     @Severity(SeverityLevel.NORMAL)
     @Description("The \"Wspólnota Matki Miłosiernego\" link")
     @QaseId(34)
     @QaseTitle("The \"Wspólnota Matki Miłosiernego\" link")
     public void mothersCommunityLink() {
 
-        actions(MinistrySideMenu::clickMothersCommunityLink, URLs.MOTHERS_COMMUNITY.getName());
+        actions(MinistrySideMenu::clickMothersCommunityLink, URLs.MOTHERS_COMMUNITY);
     }
 }
