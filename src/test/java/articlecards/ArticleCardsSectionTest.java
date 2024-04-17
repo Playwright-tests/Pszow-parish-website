ec9385e6e89def76536688e3bd926a12588beb52
pawelaksman <pablo_xmen@op.pl>
2024-04-17 23:29:53 +0200

    Added annotations

--- src/test/java/articlecards/ArticleCardsSectionTest.java
+++ src/test/java/articlecards/ArticleCardsSectionTest.java
@@ -1,15 +1,18 @@
 package articlecards;
 
+import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.options.AriaRole;
 import io.qameta.allure.*;
 import io.qase.api.annotation.QaseId;
 import io.qase.api.annotation.QaseTitle;
+import org.junit.jupiter.api.Tag;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import qa.base.BaseTest;
-import qa.enums.URLs;
+import qa.support.URLs;
+import qa.support.AllureAttachments;
 
 @Epic("E2E")
 @Feature("The article cards section")
@@ -18,17 +21,21 @@ public class ArticleCardsSectionTest extends BaseTest {
     @BeforeMethod
     public void create() {
 
-        goToPage(URLs.BASE_URL.getName());
+        goToPage(URLs.HOME_PAGE);
     }
 
     @io.qameta.allure.Step("Click the link")
     @io.qase.api.annotation.Step("Click the link")
     private void clickLink() {
 
-        getPage().getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Zobacz więcej aktualności ...")).click();
+        Locator link = getPage().getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Zobacz więcej aktualności ..."));
+        AllureAttachments.takeScreenshot(link, "seeMoreLink");
+        link.click();
     }
 
     @Test
+    @Tag("Links")
+    @Owner("Paweł Aksman")
     @Severity(SeverityLevel.CRITICAL)
     @Description("The \"Zobacz więcej aktualności ...\" link")
     @QaseId(46)
@@ -36,6 +43,6 @@ public class ArticleCardsSectionTest extends BaseTest {
     public void newsLink() {
 
         clickLink();
-        Assert.assertEquals(getPage().url(), URLs.NEWS_PAGE.getName(), "The new page has not been opened");
+        Assert.assertEquals(getPage().url(), URLs.NEWS_PAGE, "The new page has not been opened");
     }
 }
